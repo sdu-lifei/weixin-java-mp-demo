13a10d83cd6fe5cf4485087e313c9164a8d8deb9
sdu-lifei <sdu-lifei@163.com>
2023-12-02 22:52:49 +0800

    reply message

--- src/main/java/com/github/binarywang/demo/wx/mp/handler/MsgHandler.java
+++ src/main/java/com/github/binarywang/demo/wx/mp/handler/MsgHandler.java
@@ -1,7 +1,6 @@
 package com.github.binarywang.demo.wx.mp.handler;
 
 import com.github.binarywang.demo.wx.mp.builder.TextBuilder;
-import com.github.binarywang.demo.wx.mp.utils.JsonUtils;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.common.session.WxSessionManager;
 import me.chanjar.weixin.mp.api.WxMpService;
@@ -11,8 +10,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
-
-import static me.chanjar.weixin.common.api.WxConsts.XmlMsgType;
 
 /**
  * @author <a href="https://github.com/binarywang">Binary Wang</a>
@@ -25,9 +22,9 @@ public class MsgHandler extends AbstractHandler {
                                     Map<String, Object> context, WxMpService weixinService,
                                     WxSessionManager sessionManager) {
 
-        if (!wxMessage.getMsgType().equals(XmlMsgType.EVENT)) {
-            //TODO 可以选择将消息保存到本地
-        }
+//        if (!wxMessage.getMsgType().equals(XmlMsgType.EVENT)) {
+//            //TODO 可以选择将消息保存到本地
+//        }
 
         //当用户输入关键词如“你好”，“客服”等，并且有客服在线时，把消息转发给在线客服
         try {
@@ -43,7 +40,9 @@ public class MsgHandler extends AbstractHandler {
         }
 
         //TODO 组装回复消息
-        String content = SearchServiceImpl.searchByKeyword(wxMessage.getContent());
+        String content = wxMessage.getContent();
+        logger.info("get message: " + content);
+        content = SearchServiceImpl.searchByKeyword(wxMessage.getContent());
         return new TextBuilder().build(content, wxMessage, weixinService);
 
     }
